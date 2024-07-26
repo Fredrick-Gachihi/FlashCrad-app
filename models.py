44ebb23df95e408d759be102602ed37b8cebb09a
fredrick-gachihi <fredrickgachihi24@gmail.com>
2024-07-26 13:15:04 +0300

    models.py

---
+++ models.py
@@ -0,0 +1,59 @@
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import validates
+from sqlalchemy import MetaData
+ 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///deck_portal.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.column(db.integer, primary_key= True)
+    username = db.column(db.string(90), nullable =False, unique =True)
+    email = db.column(db.string(90), nullable =False  False, unique True)
+    password_hash = db.column(db.strimg(90), nullable = False)
+
+    @validates('email')
+    def validate_email(self, key, address):
+        assert '@'in address
+        return address
+
+class Deck(db.Model):
+    __tablename__ = 'decks'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    deck_name = db.Column(db.String(255), nullable=False)
+    
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    user = db.relationship('User', backref=db.backref('decks', lazy=True))
+
+    
+
+class StudySession(db.Model):
+    __tablename__ = 'studySessions'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    deck_id = db.Column(db.Integer, db.ForeignKey('decks.deck_id'), nullable=False)
+    session_date = db.Column(db.Date, nullable=False)
+    score = db.Column(db.Integer)
+
+    user = db.relationship('User', backref=db.backref('studysessions', lazy=True))
+    deck = db.relationship('Deck', backref=db.backref('studysessions', lazy=True))
+
+class FlashCard(db.model):
+    __tablename__ = 'flashCards'    
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    deck_id = db.Column(db.Integer, db.ForeignKey('decks.deck_id'), nullable=False)
+    question = db.Column(db.Text, nullable=False)
+    answer = db.Column(db.Text, nullable=False)
+
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    deck = db.relationship('Deck', backref=db.backref('flashcards', lazy=True))
+
+if __name__ == '__main__':
+    app.run(debug=True)   
