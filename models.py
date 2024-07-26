37841b04e70c7a6286ccaa57b2b31a64389ae9c3
fredrick-gachihi <fredrickgachihi24@gmail.com>
2024-07-26 13:50:33 +0300

    models.py

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
 from sqlalchemy import MetaData
+import bcrypt 
  
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///deck_portal.db'
@@ -19,6 +20,11 @@ class User(db.Model):
     def validate_email(self, key, address):
         assert '@'in address
         return address
+    
+    @validates('username')
+    def validate_username(self, key, username):
+        assert len(username) <= 90, "Username characters should be less than ninety."
+        return username
 
 class Deck(db.Model):
     __tablename__ = 'decks'
@@ -26,7 +32,7 @@ class Deck(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
     deck_name = db.Column(db.String(255), nullable=False)
-    
+
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
     user = db.relationship('User', backref=db.backref('decks', lazy=True))
 
@@ -54,6 +60,21 @@ class FlashCard(db.model):
 
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
     deck = db.relationship('Deck', backref=db.backref('flashcards', lazy=True))
+
+
+@app.route('/users', method =['POST'])
+def create_user():
+    data = request.get_json()
+    new_user = User(username=data['username'], email=data['email'], password_hash=data['password_hash'])
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"message": "User created"}), 201
+ 
+@app.route('/user', method =['GET'])
+def get_deck():
+    deck = Deck.query.all()
+    return jsonify([deck.to_dict() for deck in decks])
+
 
 if __name__ == '__main__':
     app.run(debug=True)   
