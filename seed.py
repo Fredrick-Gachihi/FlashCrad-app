dc7120cb4b6c181e6b9bb675f03d47d2a5f1f2fd
fredrick-gachihi <fredrickgachihi24@gmail.com>
2024-07-26 16:39:09 +0300

    seed.py

--- seed.py
+++ seed.py
@@ -6,18 +6,47 @@ from models import db, Deck, User, StudySession, FlashCard
 with app.py_context():
     faker = Faker()
 
+    FlashCard.querry.delete()
     Deck.querry.delete()
+    flashCards = []
     decks = []
+    for User in users:
+        for _ in range(3):
+            deck_name =faker.deck_name()
+            userid =faker.user_id()
+            
+            decks.append(Deck)
+
+            db.session.add_all(Deck)
+            db.session.commit()
+
+        for _ in range(5):
+            deck_id = faker.deck_id
+            question = faker.sentense()
+            answer = faker.paragraph
+
+            flashCards.append(FlashCard)
+
+            db.session.add_all(FlashCard)
+            db.session.commit    
 
     User.querry.delete()
     users = []
+    for _ in range(10):
+        username = faker.user_name()
+        email =f"{username}@{domain}"
+        password_hash =faker.password()
 
+        existing_user = User.query.filter_by(email=email).first()
+        if existing_user:
+            continue
+        users.append(User)
+        
+        db.session.add_all(User)
+        db.session.commit()
 
     StudySession.querry.delete()
     studySessions = []
-
-    FlashCard.querry.delete()
-    flashCards = []
 
 
 
