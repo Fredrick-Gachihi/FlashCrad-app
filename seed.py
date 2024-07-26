90acd0fe421fb98c2c5713f1976fa7ef774f7bf2
fredrick-gachihi <fredrickgachihi24@gmail.com>
2024-07-26 14:39:41 +0300

    seed.py

--- seed.py
+++ seed.py
@@ -6,7 +6,19 @@ from models import db, Deck, User, StudySession, FlashCard
 with app.py_context():
     faker = Faker()
 
-    
+    Deck.querry.delete()
+    decks = []
+
+    User.querry.delete()
+    users = []
+
+
+    StudySession.querry.delete()
+    studySessions = []
+
+    FlashCard.querry.delete()
+    flashCards = []
+
 
 
 print("Data created successfully.")
